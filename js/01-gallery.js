2ac2d83511f617751f2c31ee1726b3540dea94f4
Eduard <sorokolietov@gmail.com>
2023-08-17 10:30:10 +0200

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+let ID = 0;
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryRef.addEventListener('click', onGalleryImgClick);
+
+function createGalleryMarkup(arr) {
+  return arr.map(item => 
+    `<li class="gallery__item">
+      <a class="gallery__link">
+        <img
+          class="gallery__image"
+          src="${item.preview}" 
+          data-source="${item.original}"
+          alt="${item.description}">
+      </a>
+    </li>`
+  ).join('');
+};
+
+function onGalleryImgClick(e) {
+
+  if (e.currentTarget === e.target) {
+    return;
+  }
+
+  const image = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<div class="modal"><img width="100%" src="${image}"></div>`, {
+      onShow: (newInst) => {
+        newInst.element().addEventListener('click', () => instance.close());
+      },
+    }
+  );
+  
+  instance.show();
+}
